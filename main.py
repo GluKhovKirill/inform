d397661790a49cfc69e625848dd1575895e1ebc8
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2021-09-16 06:16:04 -0400

    Bugdfix; added realization via lambda

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def pwr(base, pw=5):
+    flag = pw>0
     ans = 1
-    for i in range(pw):
-        ans *= base
+    
+    for i in range(abs(pw)):
+        ans = ans*base if flag else ans/base
+        
     return ans
 
 
+pwr_via_lambda = lambda base,power: base**power
+
 if __name__ == "__main__":
     print(pwr(2, 3))
+    print(pwr_via_lambda(2,5))
