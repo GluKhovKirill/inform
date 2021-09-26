09dd56d9f9843a0a313e69c8c83fa82cafc57a32
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2021-09-26 19:47:04 +0300

    Beautifyed a bit

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 def pwr(base, pw=5):
     ans = 1
-    
     for i in range(abs(pw)):
         ans = ans*base if (pw>0) else ans/base
         
     return ans
 
 
-pwr_via_lambda = lambda base,power: base**power
-
 if __name__ == "__main__":
     print(pwr(2, 3))
-    print(pwr_via_lambda(2,5))
