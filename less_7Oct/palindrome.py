77042aee6ac88cd6e2399297b9cb3b49d347c6e1
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2021-10-07 15:33:35 +0300

    Added "is_palindrome" function call with user input

--- less_7Oct/palindrome.py
+++ less_7Oct/palindrome.py
@@ -2,3 +2,7 @@ def is_palindrome(string):
     return string == string[::-1]
 
 
+if __name__ == '__main__':
+    print("Enter the phrase:")
+    print("It's a palindrome!" if is_palindrome(input(">>")) else "It's not a palindrome!")
+    
